06d586ee085d6d3f7920ef163a66a2f2869d4263
lokeshkhanal <60536520+lokeshkhanal@users.noreply.github.com>
2023-03-04 20:01:19 -0500

    Initial Commit to Chatting APP

---
+++ app/src/main/java/ca/lokendra/chattingapp/Signup.kt
@@ -0,0 +1,11 @@
+package ca.lokendra.chattingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
